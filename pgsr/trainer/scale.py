55fa3bfb835f1679727be7bb2d39637772ebf012
yindaheng98 <yindaheng98@gmail.com>
2026-08-02 16:12:34 -0700

    fixed NaN when visibility_filter is all false

--- pgsr/trainer/scale.py
+++ pgsr/trainer/scale.py
@@ -16,7 +16,7 @@ class PlanarScaleTrainer(TrainerWrapper):
     def loss(self, out: dict, camera: Camera) -> torch.Tensor:
         loss = super().loss(out, camera)
         visibility_filter = out["visibility_filter"].reshape(-1)
-        if visibility_filter.numel() == 0:
+        if visibility_filter.sum() == 0:
             return loss
 
         # Source: https://github.com/zju3dv/PGSR/blob/de24f1a38b350387e8d8fe381b2cd70c1ae946e7/train.py#L177-L182
